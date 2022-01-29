a96a7788f00964be7087cb979ac7c94ae1af48f2
mario-ragtag <mario@ragtagagency.com>
2022-01-29 23:05:08 +0100

    Prototipo meccaniche

---
+++ Assets/AttractRepelAction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof (SphereCollider))]
+public class AttractRepelAction : MonoBehaviour
+{
+    [SerializeField] KeyCode key;
+    public float force;
+    public float radius = 5;
+    [SerializeField] Collider [] Colliders;
+
+    private void Start()
+    {
+        GetComponent<SphereCollider>().radius = radius;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(key))
+            Action();
+    }
+
+    private void Action()
+    {
+        Debug.Log("Boom");
+        Colliders = Physics.OverlapSphere(transform.position, radius);
+        Rigidbody r;
+        foreach (Collider c in Colliders)
+        {
+            if(c.tag == "Pecora")
+            {
+                r = c.GetComponent<Rigidbody>();
+                r.AddExplosionForce(force, transform.position, radius);
+            }
+        }
+    }
+}
---
+++ Assets/RandomRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour
+{
+    private void Start()
+    {
+        transform.rotation = Quaternion.Euler(  transform.rotation.x,
+                                                Random.RandomRange(0, 360),
+                                                transform.rotation.z);
+    }
+}
