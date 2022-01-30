559222d6f1cf66b606a32f97bde3cd63af618a46
mario-ragtag <mario@ragtagagency.com>
2022-01-30 15:17:22 +0100

    Final. Maybe.

--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -10,6 +10,13 @@ public class GameOver : MonoBehaviour
     [SerializeField] Text fraseWin;
     [SerializeField] Porta portaLupo;
     [SerializeField] Porta portaCane;
+    [SerializeField] GameObject portraitCane;
+    [SerializeField] GameObject portraitLupo;
+
+    [SerializeField] AudioSource source;
+    [SerializeField] AudioClip sfxLupo;
+    [SerializeField] AudioClip sfxCane;
+
     public void SetGameOver()
     {
         Debug.Log("GameOver");
@@ -22,11 +29,20 @@ public class GameOver : MonoBehaviour
             go.SetActive(false);
         }
 
+        fraseWin.text = $"The vegan wolf against animal cruelty has successfully rescued {portaLupo.GetPunteggio()} sheeps!";
+        fraseWin.text += $"\nThe guard dog, blind follower of his master's order, has successfully put in jail {portaCane.GetPunteggio()} sheeps!";
+
         if (portaLupo.GetPunteggio() > portaCane.GetPunteggio())
         {
-            fraseWin.text = "The vegan wolf against animal cruelty\nhas successfully rescued the sheeps!";
+            source.clip = sfxLupo;
+            source.Play();
+            portraitCane.transform.localScale = new Vector3(.6f, .6f, .6f);
         }
-        else
-            fraseWin.text = "The guard dog, blind follower of his master's order,\nhas successfully put in jail the sheeps!";
+        else if (portaLupo.GetPunteggio() < portaCane.GetPunteggio())
+        {
+            source.clip = sfxCane;
+            source.Play();
+            portraitLupo.transform.localScale = new Vector3(.6f, .6f, .6f);
+        }        
     }
 }
